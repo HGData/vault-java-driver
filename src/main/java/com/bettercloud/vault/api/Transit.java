43a7f2de192260c8d5e620c5819fcf7e10cb69c1
AlexSHG <alex.schreifels@hgdata.com>
2019-05-03 14:54:31 -0700

    WIP creating Transit API

--- src/main/java/com/bettercloud/vault/api/Transit.java
+++ src/main/java/com/bettercloud/vault/api/Transit.java
@@ -1,6 +1,16 @@
 package com.bettercloud.vault.api;
 
 import com.bettercloud.vault.VaultConfig;
+import com.bettercloud.vault.VaultException;
+import com.bettercloud.vault.json.JsonObject;
+import com.bettercloud.vault.response.TransitResponse;
+import com.bettercloud.vault.rest.Rest;
+import com.bettercloud.vault.rest.RestException;
+import com.bettercloud.vault.rest.RestResponse;
+
+import java.io.UnsupportedEncodingException;
+import java.nio.charset.StandardCharsets;
+import java.util.Base64;
 
 /**
  * <p>The implementing class for Vault's Transit Engine operations (e.g. encrypt, decrypt).</p>
@@ -11,11 +21,41 @@ import com.bettercloud.vault.VaultConfig;
 public class Transit {
     private final VaultConfig config;
 
-    private String keyRing;
-
     public enum transitOperations {encrypt, decrypt}
 
     public Transit(VaultConfig config) {
         this.config = config;
+    }
+
+    public TransitResponse encrypt(String keyRing, String data) {
+        int retryCount = 0;
+        while (true) {
+            try {
+                JsonObject postBody = new JsonObject();
+                JsonObject plaintext = new JsonObject().add("plaintext", Base64.getEncoder().encodeToString(data.getBytes(StandardCharsets.UTF_8)));
+                postBody.add("data", plaintext);
+
+                final RestResponse response = new Rest()
+                        .url(config.getAddress() + "/v1/" + keyRing)
+                        .header("X-Vault-Token", config.getToken())
+                        .body(postBody.toString().getBytes(StandardCharsets.UTF_8))
+                        .connectTimeoutSeconds(config.getOpenTimeout())
+                        .readTimeoutSeconds(config.getReadTimeout())
+                        .sslVerification(config.getSslConfig().isVerify())
+                        .sslContext(config.getSslConfig().getSslContext())
+                        .post();
+
+                // Validate response
+                if (response.getStatus() != 200) {
+                    throw new VaultException("Vault responded with HTTP status code: " + response.getStatus()
+                            + "\nResponse body: " + new String(response.getBody(), StandardCharsets.UTF_8),
+                            response.getStatus());
+                }
+
+                return new TransitResponse(response, retryCount, transitOperations.encrypt);
+            } catch (RuntimeException | VaultException | RestException e) {
+
+            }
+        }
     }
 }
