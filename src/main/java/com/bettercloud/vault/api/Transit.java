2022540861f2584e7e1da698b976a9c825c36529
Chad Simmons <chad.simmons@hgdata.com>
2019-05-13 13:49:02 -0700

    CA-783 drying up Transit methods via refactor

--- src/main/java/com/bettercloud/vault/api/Transit.java
+++ src/main/java/com/bettercloud/vault/api/Transit.java
@@ -27,34 +27,36 @@ public class Transit {
     }
 
     public TransitResponse encrypt(String keyRing, String data) throws VaultException {
+        String postBody = Json.object().add("plaintext", Base64.getEncoder().encodeToString(data.getBytes(StandardCharsets.UTF_8))).toString();
+        return makeTransitRequestWithRetries(keyRing, transitOperations.encrypt, postBody, config.getMaxRetries(), config.getRetryIntervalMilliseconds());
+    }
+
+    public TransitResponse decrypt(String keyRing, String cipherText) throws VaultException {
+        String postBody = Json.object().add("ciphertext", cipherText).toString();
+        return makeTransitRequestWithRetries(keyRing, transitOperations.decrypt, postBody, config.getMaxRetries(), config.getRetryIntervalMilliseconds());
+    }
+
+    private TransitResponse makeTransitRequestWithRetries(String keyRing,
+                                                          transitOperations operation,
+                                                          String postBody,
+                                                          int maxRetryCount,
+                                                          int retryIntervalMilliseconds) throws VaultException {
         int retryCount = 0;
         while (true) {
             try {
-                String postBody = Json.object().add("plaintext", Base64.getEncoder().encodeToString(data.getBytes(StandardCharsets.UTF_8))).toString();
+                final RestResponse response = makeTransitRestRequest(keyRing, operation, postBody);
 
-                final RestResponse response = new Rest()
-                        .url(config.getAddress() + "/v1/transit/encrypt/" + keyRing)
-                        .header("X-Vault-Token", config.getToken())
-                        .body(postBody.getBytes(StandardCharsets.UTF_8))
-                        .connectTimeoutSeconds(config.getOpenTimeout())
-                        .readTimeoutSeconds(config.getReadTimeout())
-                        .sslVerification(config.getSslConfig().isVerify())
-                        .sslContext(config.getSslConfig().getSslContext())
-                        .post();
-
-                // Validate response
                 if (response.getStatus() != 200) {
                     throw new VaultException("Vault responded with HTTP status code: " + response.getStatus()
-                            + "\nResponse body: " + new String(response.getBody(), StandardCharsets.UTF_8),
-                            response.getStatus());
+                      + "\nResponse body: " + new String(response.getBody(), StandardCharsets.UTF_8),
+                      response.getStatus());
                 }
 
-                return new TransitResponse(response, retryCount, transitOperations.encrypt);
+                return new TransitResponse(response, retryCount, transitOperations.decrypt);
             } catch (RuntimeException | VaultException | RestException e) {
-                if (retryCount < config.getMaxRetries()) {
+                if (retryCount < maxRetryCount) {
                     retryCount++;
                     try {
-                        final int retryIntervalMilliseconds = config.getRetryIntervalMilliseconds();
                         Thread.sleep(retryIntervalMilliseconds);
                     } catch (InterruptedException e1) {
                         e1.printStackTrace();
@@ -69,45 +71,22 @@ public class Transit {
         }
     }
 
-    public TransitResponse decrypt(String keyRing, String cipherText) throws VaultException {
-        int retryCount = 0;
-        while (true) {
-            try {
-                String postBody = Json.object().add("ciphertext", cipherText).toString();
+    private RestResponse makeTransitRestRequest(String keyRing, transitOperations operation, String postBody) throws RestException {
+        return new Rest()
+          .url(config.getAddress() + "/v1/transit/" + operationURIEndpoint(operation) + "/" + keyRing)
+          .header("X-Vault-Token", config.getToken())
+          .body(postBody.getBytes(StandardCharsets.UTF_8))
+          .connectTimeoutSeconds(config.getOpenTimeout())
+          .readTimeoutSeconds(config.getReadTimeout())
+          .sslVerification(config.getSslConfig().isVerify())
+          .sslContext(config.getSslConfig().getSslContext())
+          .post();
+    }
 
-                final RestResponse response = new Rest()
-                        .url(config.getAddress() + "/v1/transit/decrypt/" + keyRing)
-                        .header("X-Vault-Token", config.getToken())
-                        .body(postBody.getBytes(StandardCharsets.UTF_8))
-                        .connectTimeoutSeconds(config.getOpenTimeout())
-                        .readTimeoutSeconds(config.getReadTimeout())
-                        .sslVerification(config.getSslConfig().isVerify())
-                        .sslContext(config.getSslConfig().getSslContext())
-                        .post();
-
-                if (response.getStatus() != 200) {
-                    throw new VaultException("Vault responded with HTTP status code: " + response.getStatus()
-                            + "\nResponse body: " + new String(response.getBody(), StandardCharsets.UTF_8),
-                            response.getStatus());
-                }
-
-                return new TransitResponse(response, retryCount, transitOperations.decrypt);
-            } catch (RuntimeException | VaultException | RestException e) {
-                if (retryCount < config.getMaxRetries()) {
-                    retryCount++;
-                    try {
-                        final int retryIntervalMilliseconds = config.getRetryIntervalMilliseconds();
-                        Thread.sleep(retryIntervalMilliseconds);
-                    } catch (InterruptedException e1) {
-                        e1.printStackTrace();
-                    }
-                } else if (e instanceof VaultException) {
-                    // ... otherwise, give up.
-                    throw (VaultException) e;
-                } else {
-                    throw new VaultException(e);
-                }
-            }
+    private String operationURIEndpoint(Transit.transitOperations operation) {
+        switch (operation) {
+            case decrypt: return "decrypt";
+            case encrypt: return "encrypt";
         }
     }
 }
